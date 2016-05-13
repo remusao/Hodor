6486c282f828072def2e3676f6f2e3058401a380
Vincent Latrouite <vincent.latrouite@gmail.com>
2016-05-13 10:50:43 +0200

    [language][buffalo] fixed the buffalo file to be the same at the others for the last version.

--- src/Language/Brainfuck/Buffalo.hs
+++ src/Language/Brainfuck/Buffalo.hs
@@ -1,45 +1,18 @@
 
 module Language.Brainfuck.Buffalo where
 
-import Prelude hiding (read, print)
+import Prelude ()
+import Language.Brainfuck.Internals.GenericParser
 
-import Control.Monad (liftM)
-import Data.Maybe (catMaybes)
-import Text.ParserCombinators.Parsec
-import Language.Brainfuck.Instructions
-
-parseOp :: String -> String -> Instr -> Parser (Maybe Instr)
-parseOp s1 s2 instr = do
-    _ <- string s1
-    _ <- skipMany comment
-    _ <- string s2
-    return . Just $ instr
-    
-incr = parseOp "buffalo" "buffalo" Incr
-decr = parseOp "buffalo" "Buffalo" Decr
-right = parseOp "Buffalo" "buffalo" MoveRight
-left = parseOp "Buffalo" "Buffalo" MoveLeft
-read = parseOp "Buffalo" "Buuffalo" Read
-print = parseOp "buffalo" "Buuffalo" Print
-eop = parseOp "Buuffalo" "Buuffalo" Nope
-
-instr :: Parser (Maybe Instr)
-instr = (try incr <|>
-         try decr <|>
-         try right <|>
-         try left <|>
-         try read <|>
-         try print)
-
-loop :: Parser (Maybe Instr)
-loop = between open close (program >>= return . Just . Loop)
-    where
-        open = (try $ parseOp "buffalo" "buffalo" Nope)
-        close = (try $ parseOp "Buuffalo" "Buuffalo" Nope)
-
-comment :: Parser (Maybe Instr)
-comment = noneOf "bBuffalo" >> return Nothing
-
-program :: Parser [Instr]
-program = liftM catMaybes $ many $ instr <|> loop <|> comment
-
+program :: GenericParser
+program = genparser Symbols {
+    incr     = "buffalo buffalo",
+    decr     = "buffalo Buffalo",
+    right    = "Buffalo buffalo",
+    left     = "Buffalo Buffalo",
+    read     = "Buffalo Buuffalo",
+    print    = "buffalo Buuffalo",
+    openl    = "Buuffalo Buuffalo",
+    closel   = "Buuffalo buuffalo",
+    reserved = "bBuffalo"
+}
